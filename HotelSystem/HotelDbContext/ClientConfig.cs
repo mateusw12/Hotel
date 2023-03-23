555c439f81722a3c69ad880d5c5ce31d723c944b
Mateus Walz <mateus.walz@gmail.com>
2023-03-23 10:14:46 -0300

    Implementa contexto do banco de dados

---
+++ HotelSystem/HotelDbContext/ClientConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using HotelSystem.Model;
+
+namespace HotelSystem.HotelDbContext
+{
+    public class ClientConfig : EntityTypeConfiguration<Client>
+    {
+        public ClientConfig()
+        {
+            Property(client => client.Account).IsOptional().HasMaxLength(20);
+            HasRequired(client => client.Room).WithMany(room => room.Clients).WillCascadeOnDelete(true);
+            
+            ToTable("Clients");
+        }
+    }
+}
---
+++ HotelSystem/HotelDbContext/HotelContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using HotelSystem.Model;
+
+namespace HotelSystem.HotelDbContext
+{
+    public class HotelContext : DbContext
+    {
+        public HotelContext(string connectionString = "HotelDbConnectionString")
+            : base(connectionString)
+        {
+            
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.Add(new PersonConfig());
+            modelBuilder.Configurations.Add(new ClientConfig());
+            modelBuilder.Configurations.Add(new RoomConfig());
+        }
+    }
+}
